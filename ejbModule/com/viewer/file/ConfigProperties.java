34edc3840163532419399686bfa266fed2ceae51
Arthur Xu <r2ysxu@gmail.com>
2015-06-18 15:49:19 -0400

    uses config file in server config directory

--- ejbModule/com/viewer/file/ConfigProperties.java
+++ ejbModule/com/viewer/file/ConfigProperties.java
@@ -1,34 +1,27 @@
 package com.viewer.file;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 public class ConfigProperties {
 	
 	private static Properties prop = new Properties();
 	
-	/* static {
+	static {
 		InputStream is = null;
 		try {
-			ConfigProperties.class.getClassLoader();
-			is =  ClassLoader.getSystemResourceAsStream("config/config.properties");
-			if (is != null) {
-				prop.load(is);
-			} else System.out.println("Can't find Config file");
+			is = new FileInputStream(new File("config.properties"));
+			prop.load(is);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-	} */
+	}
 
 	public static String getProperty(String key) {
-		//String property = prop.getProperty(key);
-		if (key == null) return null;
-		if (key.equals("jdbcLoc")) {
-			return "jdbc:sqlite:E:/PictoViewDB/database/pdb.db";
-		} else if (key.equals("albumDirectory")) {
-			return "E:/PictoViewDB/User/Albums/";
-		} else if (key.equals("thumbnailDirectory")) {
-			return "E:/PictoViewDB/User/Thumbnail/";
-		}
-		return null;
+		String property = prop.getProperty(key);
+		return property;
 	}
 }
